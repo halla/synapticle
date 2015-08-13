12bda6dad45eb96817cabb10b2f4b3d07f979ae6
Antti Halla <antti.halla@gmail.com>
2015-08-13 20:41:28 +0300

    Fix single mode and switching back to drizzle

--- src/app/players.cljs
+++ src/app/players.cljs
@@ -10,12 +10,14 @@
   (reify 
     player/Player
     (step-fwd [_]
-      (reset! layout {:text ((:items (@data 0)) (mod @index (count @data)))
-                      :opacity 1.0})
+      (reset! layout [{:text ((:items (@data 0)) (mod @index (count @data)))
+                       :key (str (rand-int 100000000))
+                       :opacity 1.0}])
       (swap! index inc))
     (step-rnd [_]
-      (reset! layout  {:text (mux/get-item data) 
-                       :opacity 1.0}))
+      (reset! layout [{:text (mux/get-item data) 
+                       :key (str (rand-int 100000000))
+                       :opacity 1.0}]))
     (animation [_])
     (render [_]
       [:div {:class "single" 
@@ -24,4 +26,4 @@
                      :opacity 1.0
                      :left "40%"
                      :top "30%"}} 
-       (:text @layout)])))
+       (:text (first @layout) )])))
