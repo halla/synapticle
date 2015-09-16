30a57a13c2385853e76f8eaa192d669dbac8167d
Antti Halla <antti.halla@gmail.com>
2015-09-16 08:11:19 +0300

    Fix schema declaration

--- src/app/datasource/db.cljs
+++ src/app/datasource/db.cljs
@@ -1,8 +1,8 @@
 (ns app.datasource.db
   (:require [schema.core :as s :include-macros true]))
 
-(def schema [:title s/Str
-             :items [s/Str]])
+(def schema [{:title s/Str
+              :items [s/Str]}])
 
 (def default-value
   [{:title "Colors"
