fa69b68b976bc1661fb9828ec8a640cafeb4f911
Antti Halla <antti.halla@gmail.com>
2015-09-26 17:04:08 +0300

    Export functionality as a popover dialog

--- src/app/ctl.cljs
+++ src/app/ctl.cljs
@@ -26,7 +26,8 @@
 
 (deftmpl dataview-tpl "dataview.html")
 
-(defn import-component-body-func [submit-dialog cancel-dialog dialog-data]  
+(defn import-component-body-func 
+  [submit-dialog cancel-dialog dialog-data]  
   (fn []
     [v-box
      :children [[label
@@ -50,7 +51,35 @@
                              :on-click #(submit-dialog @dialog-data)
                              :class    "btn-primary"]]]]]))
 
-(defn popover-body
+
+(defn export-component-body-func 
+  [submit-dialog cancel-dialog dialog-data]  
+  (fn []
+    [v-box
+     :children [[label
+                 :class "help-text"
+                 :label "Copy paste the tab-indented plain string to your destination of choice."]
+                [gap :size "15px"]
+                (with-meta ;; TODO focus doesn't work
+                  [input-textarea
+                   :model            dialog-data
+                   :width            "100%"
+                   :rows             10
+                   :on-change        #(reset! dialog-data %)
+                   :change-on-blur?  true]
+                  {:component-did-mount #(do (.focus (reagent/dom-node %))
+                                             (.select (reagent/dom-node %)))})
+                [gap :size "20px"]
+                [line]
+                [gap :size "10px"]
+                [h-box
+                 :gap      "10px"
+                 :children [[button
+                             :label    [:span [:i {:class "zmdi zmdi-check" }] "OK"]
+                             :on-click #(submit-dialog @dialog-data)
+                             :class    "btn-primary"]]]]]))
+
+(defn popover-body-import
   [showing? position dialog-data on-change]
   (let [dialog-data   (reagent/atom (deref-or-value dialog-data))
         submit-dialog (fn [new-dialog-data]
@@ -68,6 +97,24 @@
        :body             [(import-component-body-func submit-dialog cancel-dialog dialog-data)]])))
 
 
+(defn popover-body-export
+  [showing? position dialog-data on-change]
+  (let [dialog-data   (reagent/atom (deref-or-value dialog-data))
+        submit-dialog (fn [new-dialog-data]
+                        (reset! showing? false)
+                        (on-change new-dialog-data))
+        cancel-dialog #(reset! showing? false)]
+    (fn []
+      [popover-content-wrapper
+       :showing?         showing?
+       :on-cancel        cancel-dialog
+       :position         position
+       :width            "400px"
+       :backdrop-opacity 0.3
+       :title            "Export all items"
+       :body             [(export-component-body-func submit-dialog cancel-dialog dialog-data)]])))
+
+
 (defn import-dlg [active-channel]
   (let [showing? (atom false)
         dlg-data (atom "")
@@ -79,7 +126,39 @@
        :anchor [button 
                 :label "Import"
                 :on-click #(reset! showing? true)]
-       :popover [popover-body showing? :right-below dlg-data on-change]])))
+       :popover [popover-body-import showing? :right-below dlg-data on-change]])))
+
+(defn channels->string 
+  "All items to tab-indented plain string list"
+  [channels]
+  (reduce (fn [%1 %2] (str %1 
+                           (:title %2) 
+                           "\n" 
+                           (reduce #(str %1 "\t" %2 "\n") 
+                                   "" 
+                                   (:items %2)))) 
+          "" 
+          channels))
+
+
+(defn export-dlg 
+  "Export all items from all channels, as tab-indented plain string list"
+  [channels]
+  (let [showing? (atom false)
+        on-change #()]
+    (fn [channels]
+      [popover-anchor-wrapper
+       :showing? showing?
+       :position :right-below
+       :anchor [button 
+                :label "Export"
+                :tooltip "Export items from all channels as plain text list"
+                :on-click #(reset! showing? true)]
+       :popover [popover-body-export
+                 showing? 
+                 :right-below 
+                 (channels->string @channels) 
+                 on-change]])))
 
 
 (defn title-input [{:keys [title on-save on-stop]}]
@@ -103,6 +182,7 @@
                                              (.select (reagent/dom-node %)))}))
 
 
+
 (defn display? [visible?]
   (if @visible?
     "display: block;"
@@ -114,7 +194,9 @@
     "hidden"))
 
 
-(defn data-item [item channel]
+(defn data-item 
+  "Single item in single channel. Editable, deletable."
+  [item channel]
   (let [editing (atom false)]
     (fn [item channel]  
       [:div {:class "channel-item"}  
@@ -186,13 +268,7 @@
                                      :label "Clear all"
                                      :on-click #(dispatch-sync [:clear-all])
                                      :tooltip "Remove all items from all channels"]]
-                               [:li [button
-                                     :label "Export"
-                                     :on-click #(dispatch-sync [:export-all])
-                                     :tooltip "Export items from all channels as plain text list"]])]                
-         ["#textarea-export" {:on-click (fn [e]
-                                          (.focus (.-target e))
-                                          (.select (.-target e)))}]))
+                               [:li [export-dlg channels]])]))
 
 (defn control-panel [playstates]
   (let [player (subscribe [:player])
